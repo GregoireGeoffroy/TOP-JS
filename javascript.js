4646e19c1b798d49f7da62eea4774d3459fafc1a
GregoireGeoffroy <thegathering92@gmail.com>
2024-07-23 19:15:17 +0300

    js function 3

--- javascript.js
+++ javascript.js
@@ -1,12 +1,22 @@
 function add7(number) {
     return number + 7;
 }
-
 console.log(add7 (2))
+
 
 function multiply(a, b) {
     return a * b;
 }
-
 console.log(multiply(3, 3))
+
+
+function capitalize(str) {
+    if (str.length === 0) return str;
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+console.log(capitalize("hello"));
+console.log(capitalize("WORLD"));
+console.log(capitalize("jAvAsCrIpT"));
+console.log(capitalize(""));
+
 
